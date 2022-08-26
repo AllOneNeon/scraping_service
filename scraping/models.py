96ab5839cb79f158aebf0b39788bf42539c7e834
Andrew Karnaushenko <allloneneon@gmail.com>
2022-08-26 12:52:37 +0300

    update service

--- scraping/models.py
+++ scraping/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=50, verbose_name= 'city name')
+    slug = models.CharField(max_length=50, blank=True)
+
+    class Meta:
+        verbose_name = 'city name'
+        verbose_name_plural = 'city names'
+    
+    def __str__(self):
+        return self.name
